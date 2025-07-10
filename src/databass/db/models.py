2c7e1672e2e8cdce0295280077b7b1669eb41fe0
hnolan <hayden@hnolan.ca>
2025-07-10 16:57:44 -0400

    Small bugfixes for exceptions on missing values in new MBZ parser functions
    Update calls to Util.get_image()
    
    Update calls to Util.get_image()
    
    Update calls to Util.get_image()

--- src/databass/db/models.py
+++ src/databass/db/models.py
@@ -523,6 +523,7 @@ class Release(MusicBrainzEntity):
 
         try:
             Util.get_image(
+                url=None,
                 entity_type="release",
                 entity_id=release_id,
                 release_name=data["name"],
